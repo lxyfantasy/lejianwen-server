a18281a9aa6febba71eae836fef937cd58d3452f
lxyfantasy <lixian1029@126.com>
2026-06-12 08:44:52 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -22,16 +22,22 @@ fn main() -> ResultType<()> {
         -r, --relay-servers=[HOST] 'Sets the default relay servers, separated by comma'
         -M, --rmem=[NUMBER(default={RMEM})] 'Sets UDP recv buffer size, set system rmem_max first, e.g., sudo sysctl -w net.core.rmem_max=52428800. vi /etc/sysctl.conf, net.core.rmem_max=52428800, sudo sysctl –p'
         , --mask=[MASK] 'Determine if the connection comes from LAN, e.g. 192.168.0.0/16'
-        -k, --key=[KEY] 'Only allow the client with the same key'",
+        -k, --key=[KEY] 'Only allow the client with the same key'
+        , --must-login=[Y|N] 'Only allow the client with login'",
     );
     init_args(&args, "hbbs", "RustDesk ID/Rendezvous Server");
-    let port = get_arg_or("port", RENDEZVOUS_PORT.to_string()).parse::<i32>()?;
+    // 默认强制登录开启
+    let mut must_login = true;
+    if let Ok(v) = get_arg("must-login") {
+        must_login = v.eq_ignore_ascii_case("y");
+    }
+    let port = get_arg_or("port", "52010".to_string()).parse::<i32>()?;
     if port < 3 {
         bail!("Invalid port");
     }
     let rmem = get_arg("rmem").parse::<usize>().unwrap_or(RMEM);
     let serial: i32 = get_arg("serial").parse().unwrap_or(0);
     crate::common::check_software_update();
-    RendezvousServer::start(port, serial, &get_arg_or("key", "-".to_owned()), rmem)?;
+    RendezvousServer::start(port, serial, &get_arg_or("key", "-".to_owned()), rmem, must_login)?;
     Ok(())
 }
